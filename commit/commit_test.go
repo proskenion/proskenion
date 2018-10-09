e6606f98b138b863ee09651ef09d3e09d76e276a
satellitex <s.a.t.e.3.ths@gmail.com>
2018-10-09 15:29:58 +0900

    test name change

--- commit/commit_test.go
+++ commit/commit_test.go
@@ -9,21 +9,7 @@ import (
 	"testing"
 )
 
-/*
-import (
-	. "github.com/proskenion/proskenion/commit"
-	. "github.com/proskenion/proskenion/test_utils"
-	"testing"
-)
-
-
-func TestCommitSystem_Commit(t *testing.T) {
-	cs := NewCommitSystem(RandomDBA(), NewTestFactory(), RandomCryptor())
-
-}
-*/
-
-func TestCommitSystem_CreateBlock(t *testing.T) {
+func TestCommitSystem_CreateBlock_Commit(t *testing.T) {
 	dba := RandomDBA()
 	fc := NewTestFactory()
 	cryptor := RandomCryptor()
