5402f8e24c99881ff81cc7289ab114e7fb6c5929
satellitex <s.a.t.e.3.ths@gmail.com>
2018-10-18 18:28:55 +0900

    todo impl consensus

--- consensus/consensus.go
+++ consensus/consensus.go
@@ -1,1 +1,51 @@
 package consensus
+
+import (
+	"github.com/inconshreveable/log15"
+	"github.com/proskenion/proskenion/commit"
+	"github.com/proskenion/proskenion/core"
+	"time"
+)
+
+type Consensus struct {
+	commitChan chan interface{}
+	cs         core.CommitSystem
+	rp         core.Repository
+	gossip     core.Gossip
+	logger     log15.Logger
+}
+
+func (c *Consensus) Boot() {
+	top, ok := c.rp.Top()
+	if !ok {
+		panic("Must be genesis commit after call this func.")
+	}
+	for {
+		timer := time.NewTimer(time.Duration(top.GetPayload().GetCreatedTime() - commit.Now()))
+		// commit を待つ
+		select {
+		case <-c.commitChan:
+			break
+		case <-timer.C:
+			break
+		}
+		top, ok = c.rp.Top()
+
+		// 1. 自分が Block の生成者か判定
+		if true {
+			// 2. block を生成
+			block, txList, err := c.cs.CreateBlock()
+			if err != nil {
+				c.logger.Error(err.Error())
+				continue
+			}
+			// 3. block を Gosship
+			err = c.gossip.GossipBlock(block, txList)
+			if err != nil {
+				c.logger.Error(err.Error())
+				continue
+			}
+		}
+	}
+
+}
--- core/p2p.go
+++ core/p2p.go
@@ -1,6 +1,8 @@
 package core
 
+import "github.com/proskenion/proskenion/core/model"
+
 // 伝搬アルゴリズム
 type Gossip interface {
-
+	GossipBlock(model.Block, TxList) error
 }
