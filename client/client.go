859651b8f4f56e15056bc312a2612301160a9f5d
satellitex <s.a.t.e.3.ths@gmail.com>
2018-10-23 16:27:48 +0900

    impl client

--- client/client.go
+++ client/client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 	"github.com/proskenion/proskenion/convertor"
+	"github.com/proskenion/proskenion/core"
 	"github.com/proskenion/proskenion/core/model"
 	"github.com/proskenion/proskenion/proto"
 	"google.golang.org/grpc"
@@ -10,18 +11,27 @@ import (
 
 type APIGateClient struct {
 	proskenion.APIGateClient
-	publicKey  model.PublicKey
-	privateKey model.PrivateKey
-	fc         model.ModelFactory
+	fc model.ModelFactory
 }
 
-func (c *APIGateClient) Write(in model.Transaction, opts ...grpc.CallOption) error {
+func NewAPIGateClient(peer model.Peer, fc model.ModelFactory) (core.APIGateClient, error) {
+	gc, err := grpc.Dial(peer.GetAddress(), grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	return &APIGateClient{
+		proskenion.NewAPIGateClient(gc),
+		fc,
+	}, nil
+}
+
+func (c *APIGateClient) Write(in model.Transaction) error {
 	tx := in.(*convertor.Transaction).Transaction
 	_, err := c.APIGateClient.Write(context.TODO(), tx)
 	return err
 }
 
-func (c *APIGateClient) Read(in model.Query, opts ...grpc.CallOption) (model.QueryResponse, error) {
+func (c *APIGateClient) Read(in model.Query) (model.QueryResponse, error) {
 	query := in.(*convertor.Query).Query
 	res, err := c.APIGateClient.Read(context.TODO(), query)
 	if err != nil {
