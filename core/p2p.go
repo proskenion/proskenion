9f0673b60fd2994f9c4c74ff586733022be8db3e
satellitex <s.a.t.e.3.ths@gmail.com>
2018-10-29 00:57:43 +0900

    impl peerservice simple

--- core/p2p.go
+++ core/p2p.go
@@ -6,3 +6,8 @@ import "github.com/proskenion/proskenion/core/model"
 type Gossip interface {
 	GossipBlock(model.Block, TxList) error
 }
+
+// Peer 取得機構
+type PeerService interface {
+	List() []model.Peer
+}
