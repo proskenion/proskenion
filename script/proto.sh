28be6b806d5ff6c4d9cde5a3e37e60de52a63f79
satellitex <s.a.t.e.3.ths@gmail.com>
2019-01-09 13:54:10 +0900

    fix proto.sh

--- script/proto.sh
+++ script/proto.sh
@@ -2,4 +2,4 @@
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 SRC=$PROJECT_ROOT/proto
 protoc --proto_path=${SRC} --go_out=plugins=grpc:${SRC} ${SRC}/*.proto
-sed -i '' "s/github.com\/golang/github.com\/satellitex/g" ${SRC}/*.pb.go
+sed -i "" "s/github.com\/golang/github.com\/satellitex/g" ${SRC}/*.pb.go
