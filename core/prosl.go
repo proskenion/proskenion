c7ed00dd659742993a7eb6fad6e58b482f30f53b
satellitex <s.a.t.e.3.ths@gmail.com>
2019-01-17 14:54:16 +0900

    main prosl struct

--- core/prosl.go
+++ core/prosl.go
@@ -3,11 +3,8 @@ package core
 import "github.com/proskenion/proskenion/core/model"
 
 type Prosl interface {
-	Convert(yaml string) error
+	ConvertFromYaml(yaml []byte) error
 	Validate() error
 	Execute() (model.Object, error)
-}
-
-type ProslConvertor interface {
-	Convert(yaml string)
+	model.Modelor
 }
