9d2684af76547a220d39ba376bc55afc8122ff13
satellitex <s.a.t.e.3.ths@gmail.com>
2019-03-12 11:09:59 +0900

    add comment

--- script/proto.sh
+++ script/proto.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 SRC=$PROJECT_ROOT/proto
-protoc --proto_path=${SRC} --go_out=plugins=grpc:${SRC} ${SRC}/*.proto
+protoc --doc_out=markdown,index.md:docs --proto_path=${SRC} --go_out=plugins=grpc:${SRC} ${SRC}/*.proto
 sed -i -e "s/github.com\/golang/github.com\/satellitex/g" ${SRC}/*.pb.go
 rm -rf ${SRC}/*-e
+sed -i -e "s/\<p\>/<pre>/g" ${PROJECT_ROOT}/docs/*
+sed -i -e "s/\<\/p\>/<\/pre>/g" ${PROJECT_ROOT}/docs/*
+rm -rf ${PROJECT_ROOT}/docs/*-e
